9c5d1594d11ba66901109f5d8a8dd86b32594855
Steven_Klavins <stevenklavins94@gmail.com>
2021-03-10 22:44:13 +0000

    feat: added destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,4 +39,13 @@ def update
       end
     end
   end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Portfolio was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
 end
