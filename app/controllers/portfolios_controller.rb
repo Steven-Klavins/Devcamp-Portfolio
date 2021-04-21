9d329757c32cb7a2fb377d617a5ac1040850060a
Steven_Klavins <stevenklavins94@gmail.com>
2021-04-21 19:43:47 +0100

    refactor: added before action for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+
   layout 'portfolio'
     def index 
         @portfolio_items = Portfolio.all
@@ -10,12 +12,9 @@ class PortfoliosController < ApplicationController
     end
 
     def show
-      @portfolio_item = Portfolio.find(params[:id])
-
     end
 
     def create
-        @portfolio_item = Portfolio.new(portfolio_params)
         respond_to do |format|
         if @portfolio_item.save
             format.html {redirect_to portfolios_path, notice: "Your portfolio item is now live"}
@@ -26,12 +25,10 @@ class PortfoliosController < ApplicationController
 end
 
 def edit
-    @portfolio_item = Portfolio.find(params[:id])
     3.times {@portfolio_item.technologies.build}
 end
 
 def update
-    @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: "portfolio_item was successfully updated." }
@@ -44,7 +41,6 @@ def update
   end
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: "Portfolio was successfully destroyed." }
@@ -57,6 +53,10 @@ def update
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body, 
       technologies_attributes: [:name])
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 end
