8e9f51ebb532583650d58124072f0b6730a77c2d
Steven_Klavins <stevenklavins94@gmail.com>
2021-04-27 23:34:51 +0100

    fix: moved blog css import to blog js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ import "channels"
 import "bootstrap"
 import "../stylesheets/application"
 
+
 document.addEventListener("turbolinks:load", () => {
     $('[data-toggle="tooltip"]').tooltip()
     $('[data-toggle="popover"]').popover()
