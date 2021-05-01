4adab9526054479d45ebfff74dc434fdcc1632d5
Steven_Klavins <stevenklavins94@gmail.com>
2021-05-01 21:55:03 +0100

    chore: added js update sort methods

--- app/javascript/packs/portfolio.js
+++ app/javascript/packs/portfolio.js
@@ -22,5 +22,37 @@ ActiveStorage.start()
 
 // On document load find each item with css class .sortable and call sortable method
 document.addEventListener('turbolinks:load', () => {
+   
     $('.sortable').sortable()
+
+    
 })
+
+function set_positons () {
+    $('.card').each(function(element) {
+        $(this).attr('data-pos'), element + 1
+        return
+    });
+  }
+
+  $(window).on("load", function() {
+      console.log("called")
+    set_positons ()
+
+    $('.sortable').sortable()
+
+    $('.sortable').sortable().bind('sortupdate', function (e, ui) {
+        let updated_order = []
+        set_positons ()
+        $('.card').each(function(element) {
+           updated_order.push({id:$(this).data('id'), position: element+1 }) 
+        })
+
+        $.ajax({ 
+            type: 'PUT',
+            url: 'portfolios/sort',
+            data: {order: updated_order}
+          });
+    });   
+    //dom is changed 
+});
