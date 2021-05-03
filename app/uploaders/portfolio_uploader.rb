8d56bcf71398467e2bb6c8f2c2819142919748a9
Steven_Klavins <stevenklavins94@gmail.com>
2021-05-03 16:23:13 +0100

    chore: generated carrier upload config

---
+++ app/uploaders/portfolio_uploader.rb
@@ -0,0 +1,16 @@
+class PortfolioUploader < CarrierWave::Uploader::Base
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+
+  # Add an allowlist of extensions which are allowed to be uploaded.
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
+
+
+end
