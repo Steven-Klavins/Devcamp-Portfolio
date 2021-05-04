83969032cb2e4fb913471578facbfc4d77289b8f
Steven_Klavins <stevenklavins94@gmail.com>
2021-05-04 05:03:33 +0100

    chore: required cocoon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "../stylesheets/application"
+require("@nathanvda/cocoon")
 
 
 document.addEventListener("turbolinks:load", () => {
