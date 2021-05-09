9f92fa4d40f596fa56be918989f1d3843a03433c
Steven_Klavins <stevenklavins94@gmail.com>
2021-05-09 01:39:13 +0100

    fix: social tool now returns full string

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -7,8 +7,15 @@ module SocialTool
       config.access_token_secret = ENV.fetch("TWITTER_ACCESS_TOKEN_SECRET")
     end
 
-    client.search("#rails", result_type: 'recent').take(6).collect do |tweet|
-        "#{tweet.user.screen_name}: #{tweet.text}"
+    client.search("#coding", result_type: 'recent', tweet_mode: 'extended').take(6).collect do |tweet|
+        if tweet.truncated? && tweet.attrs[:extended_tweet]
+    "#{tweet.user.screen_name}: #{tweet.attrs[:extended_tweet].full_text}" 
+else
+    # REST API with extended mode, or untruncated text in Streaming API
+    "#{tweet.user.screen_name}: #{tweet.full_text}" 
+end
     end
   end
 end
+
+
