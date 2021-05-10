fcab85bc934d9269b91ea7521fa321ec0f310e2e
Steven_Klavins <stevenklavins94@gmail.com>
2021-05-10 07:30:58 +0100

    chore: added action cable for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,18 @@
 class CommentsController < ApplicationController
-    def create 
-        @comment = current_user.comments.build(comment_params)
-    end
-end
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @comment.update(blog_id: params[:blog_id]) # ID passed form comments form
 
-def comment_params 
+    if @comment.save
+      ActionCable.server.broadcast "BlogsChannel",
+                                   { content: @comment.content }
+    end
+    head :no_content
+  end
+
+  private
+
+  def comment_params
     params.require(:comment).permit(:content)
+  end
 end
