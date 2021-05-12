e710288e206534f63da761f1bf1e794c3264c02e
Steven_Klavins <stevenklavins94@gmail.com>
2021-05-12 21:42:16 +0100

    chore: called custom scope for topics sidebar

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,12 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [ :show, :edit, :update, :destroy, :toggle_status]
-  layout 'blog'
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
+  layout "blog"
+  access all: [:show, :index], user: { except: [:destroy, :new, :create, :update, :edit, :toggle_status] }, site_admin: :all
 
   def index
     if logged_in?(:site_admin)
-    @blogs = Blog.most_recent_first.page(params[:page]).per(5)
+      @blogs = Blog.most_recent_first.page(params[:page]).per(5)
     else
       @blogs = Blog.published.most_recent_first.page(params[:page]).per(5)
     end
@@ -14,10 +15,10 @@ class BlogsController < ApplicationController
 
   def show
     if logged_in?(:site_admin) || @blog.published?
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
-    @page_title = @blog.title
-    @seo_keywords = @blog.body
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
+      @page_title = @blog.title
+      @seo_keywords = @blog.body
     else
       redirect_to blogs_path
     end
@@ -65,25 +66,28 @@ class BlogsController < ApplicationController
   end
 
   def toggle_status
-    
-  if  @blog.draft?
-      @blog.published! 
-  
-  elsif @blog.published?
-    @blog.draft! 
-  end
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
 
-    redirect_to blogs_url notice: "Post status has been updated" 
+    redirect_to blogs_url notice: "Post status has been updated"
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.friendly.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def blog_params
-      params.require(:blog).permit(:title, :body, :topic_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_blog
+    @blog = Blog.friendly.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def blog_params
+    params.require(:blog).permit(:title, :body, :topic_id)
+  end
+
+  def set_sidebar_topics
+    @side_bar_topics = Topic.with_blogs
+  end
 end
