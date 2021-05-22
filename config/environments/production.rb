645998f47d01883cd2f78f28bb4e48adc843840b
Steven_Klavins <stevenklavins94@gmail.com>
2021-05-22 14:24:33 +0100

    fix: updated action cable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'redis://redistogo:0ba72d7e4577e02e120929234a471d24@spinyfin.redistogo.com:9803/'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
