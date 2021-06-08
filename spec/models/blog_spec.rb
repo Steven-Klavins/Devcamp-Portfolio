12a39d5e88f2ef56c5c65e00d1c7fd0ca2e7e94a
Steven_Klavins <stevenklavins94@gmail.com>
2021-06-08 20:12:27 +0100

    testing: added tests for blog validation

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,12 +1,27 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Blog, type: :model do
 
-  it "Saves a valid blog to the database" do 
-    blog = Blog.new(
-      title: "My blog post", 
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit", 
-      topic_id: 1)
-      expect(blog.title).to eq("My blog post")
+  before(:all) do
+    @topic = Topic.create(id: 1, title: "Topic")
   end
+
+  it "Saves a valid blog to the database" do
+    blog = Blog.new(
+      title: "My blog post",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      topic_id: @topic.id
+    )
+    expect(blog).to be_valid
+  end
+
+  it "Does not save a blog without a valid title" do
+    blog = Blog.new(
+      title: "My blog post",
+      body: "",
+      topic_id: 1,
+    )
+    expect(blog).to_not be_valid
+  end
+
 end
