fa850e3587a02a78009befea2cf299b9be3dcdb4
Steven_Klavins <stevenklavins94@gmail.com>
2021-06-08 20:18:49 +0100

    testing: added test for invalid portfolio

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Portfolio, type: :model do
-  it "Saves a valid portfolio item to the database" do
+  it "lets the user create a valid portfolio item" do
     portfolio = Portfolio.new(
       title: "Portfolio title",
       subtitle: "Angular",
@@ -11,6 +11,19 @@ RSpec.describe Portfolio, type: :model do
       main_image: "https://via.placeholder.com/600x400",
       thumb_image: "https://via.placeholder.com/350x200",
     )
-    expect(portfolio.title).to eq("Portfolio title")
+    expect(portfolio).to be_valid
+  end
+
+  it "does not let a user create portfolio item without a valid title" do
+    portfolio = Portfolio.new(
+      title: "",
+      subtitle: "Angular",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna 
+      aliqua.",
+      main_image: "https://via.placeholder.com/600x400",
+      thumb_image: "https://via.placeholder.com/350x200",
+    )
+    expect(portfolio).to_not be_valid
   end
 end
