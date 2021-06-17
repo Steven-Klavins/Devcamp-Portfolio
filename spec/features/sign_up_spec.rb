1bf8ac9f6e23aa15b867f594e0892221ad77d95e
Steven_Klavins <stevenklavins94@gmail.com>
2021-06-17 23:55:28 +0100

    testing: added sign up feature test

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "SignUps", type: :feature do
+
+  Capybara.default_driver = :selenium_chrome
+  Capybara.current_driver = :selenium_chrome
+
+  context "A user can sign up" do 
+    scenario "A new company with valid fields should save" do
+    visit '/register'
+    fill_in "Email", with: "email@email.com"
+    fill_in "Name", with: "User"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+    expect(page).to have_content 'Welcome'
+    end
+  end
+end
