391ac198a187f6bfe7d75167628e7ae6a5327d5c
Steven_Klavins <stevenklavins94@gmail.com>
2021-06-18 23:18:27 +0100

    testing: added sign in and out test

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -14,7 +14,27 @@ RSpec.feature "SignUps", type: :feature do
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     expect(page).to have_content 'Welcome'
+    expect(page).to have_content 'Logout'
     User.last.name.should eq('User')
     end
   end
+
+  context "A user can sign in, log in and out" do 
+    scenario "Users can sign up" do
+    visit '/register'
+    fill_in "Email", with: "email@email.com"
+    fill_in "Name", with: "User"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+    click_on('Logout')
+    click_on('Login')
+    fill_in "user_email", with: "email@email.com"
+    fill_in "user_password", with: "password"
+    click_button "Log in"
+    expect(page).to have_content 'Welcome'
+    expect(page).to have_content 'Logout'
+    end
+  end
+
 end
