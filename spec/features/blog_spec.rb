42580b0b9596a407c68243896525883b6832230a
Steven_Klavins <stevenklavins94@gmail.com>
2021-06-19 18:22:55 +0100

    testing: added edit blog feature test

--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -34,5 +34,28 @@ RSpec.feature "SignUps", type: :feature do
     expect(page).to have_content 'Example Title'
     expect(page).to have_content "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     end
+
+    scenario "An admin can edit a blog" do
+    visit '/'
+    click_on('Login')
+    fill_in "user_email", with: "admin@admin.com"
+    fill_in "user_password", with: "adminpassword"
+    click_button "Log in"
+    visit '/blogs'
+    click_on('Write a New Blog')
+    fill_in "blog_title", with: "Example Title"
+    fill_in "blog_body", with: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    select "Topic 1", :from => "blog_topic_id"
+    find('[name=commit]').click
+    visit '/blogs'
+    click_on('EXAMPLE TITLE')
+    click_on('Edit')
+    fill_in "blog_body" , with: "This is the updated text for the blog example!"
+    find('[name=commit]').click
+    visit '/blogs'
+    click_on('EXAMPLE TITLE')
+    expect(page).to have_content 'Example Title'
+    expect(page).to have_content "This is the updated text for the blog example!"
+    end
   end
 end
