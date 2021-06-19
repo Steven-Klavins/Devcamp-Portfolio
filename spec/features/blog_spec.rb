e66fcac4854ff243a689312a6229ae9b25300784
Steven_Klavins <stevenklavins94@gmail.com>
2021-06-19 18:14:07 +0100

    testing: added blog feature test

---
+++ spec/features/blog_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature "SignUps", type: :feature do
+
+  Capybara.default_driver = :selenium_chrome
+  Capybara.current_driver = :selenium_chrome
+
+  before(:each) do
+    User.create(
+        email: "admin@admin.com",
+        password: "adminpassword",
+        password_confirmation: "adminpassword",
+        name: "Admin User",
+        roles: "site_admin"
+    )
+    Topic.create!(title: "Topic 1")
+  end
+
+  context "Creating and editing blogs" do 
+    scenario "An admin can create a blog" do
+    visit '/'
+    click_on('Login')
+    fill_in "user_email", with: "admin@admin.com"
+    fill_in "user_password", with: "adminpassword"
+    click_button "Log in"
+    visit '/blogs'
+    click_on('Write a New Blog')
+    fill_in "blog_title", with: "Example Title"
+    fill_in "blog_body", with: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    select "Topic 1", :from => "blog_topic_id"
+    find('[name=commit]').click
+    visit '/blogs'
+    click_on('EXAMPLE TITLE')
+    expect(page).to have_content 'Example Title'
+    expect(page).to have_content "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    end
+  end
+end
